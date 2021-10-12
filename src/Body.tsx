f6f5d399cd901ee513b4442887e33305c2f9f30a
dzgoldman <dzgoldman@wesleyan.edu>
2021-10-12 16:48:01 -0400

    template components

---
+++ src/Body.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div id="body">
+      <input id="gwei" type="number" placeholder="gwei value" />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from './Button'
+const Header  = ()=>{
+    return <header className="App-header">
+    <Button
+    text="show panel"
+    />
+    <Button
+    text="hide panel"
+    />
+  </header>
+}
+
+export default Header 
